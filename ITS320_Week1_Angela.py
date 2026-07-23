beea2bb0517381bc8deb9bed328f3dbdf1b5759f
Angi-Adema <dr.adema@gmail.com>
2026-07-23 16:22:33 -0600

    added logic to prompt user for assignment name and grades for three assignments.

--- ITS320_Week1_Angela.py
+++ ITS320_Week1_Angela.py
@@ -1,26 +1,33 @@
-#Variables to store user input for name, three assignment names, and corresponding grades
+#Variable to store user input for name
 user_name = ""
 
+#Variables to store assignment names from user input
 assignment_name_1 = ""
 assignment_name_2 = ""
 assignment_name_3 = ""
 
+#Variables to store assignment grades from user input
 assignment_grade_1 = 0.0
 assignment_grade_2 = 0.0
 assignment_grade_3 = 0.0
 
-#
+#Display a welcome message
+print("Welcome to your grade calculator!")
 
-#Prompt user for to input name, three assignment names, and corresponding grades
+#Prompt user to enter their name
 user_name = input("Please enter your first and last name: ")
 
+#Prompt user to enter first assignment name and grade
 assignment_name_1 = input("Enter the name of assignment 1: ")
 assignment_grade_1 = float(input("Enter the grade for assignment 1: "))
+
+#Prompt user to enter second assignment name and grade
 assignment_name_2 = input("Enter the name of assignment 2: ")
 assignment_grade_2 = float(input("Enter the grade for assignment 2: "))
+
+#Prompt user to enter third assignment name and grade
 assignment_name_3 = input("Enter the name of assignment 3: ")
 assignment_grade_3 = float(input("Enter the grade for assignment 3: "))
 
-#Display welcome message
-print("Welcome to your grade calculator!")
+
 
