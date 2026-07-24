7561e3e7edf7b01c2702a71f1fad0cc0794cd3e8
Angi-Adema <dr.adema@gmail.com>
2026-07-23 20:31:36 -0600

    added the total score calculation and the average score calculation as well as the formatted report

--- ITS320_Week1_Angela.py
+++ ITS320_Week1_Angela.py
@@ -1,21 +1,9 @@
-#Variable to store user input for name
-user_name = ""
-
-#Variables to store assignment names from user input
-assignment_name_1 = ""
-assignment_name_2 = ""
-assignment_name_3 = ""
-
-#Variables to store assignment grades from user input
-assignment_grade_1 = 0.0
-assignment_grade_2 = 0.0
-assignment_grade_3 = 0.0
 
 #Display a welcome message
-print("Welcome to your grade calculator!")
+print("Welcome to the grade calculator!")
 
 #Prompt user to enter their name
-user_name = input("Please enter your first and last name: ")
+user_name = input("Enter the student's first and last name: ")
 
 #Prompt user to enter first assignment name and grade
 assignment_name_1 = input("Enter the name of assignment 1: ")
@@ -29,5 +17,20 @@ assignment_grade_2 = float(input("Enter the grade for assignment 2: "))
 assignment_name_3 = input("Enter the name of assignment 3: ")
 assignment_grade_3 = float(input("Enter the grade for assignment 3: "))
 
+#Calculate the total grade score
+total_score = assignment_grade_1 + assignment_grade_2 + assignment_grade_3
+
+#Calculate the average score
+average_score = total_score / 3
+
+#Display the grade report
+print("Grade Report For:", user_name)
+print("--------------------------------")
+print(f"{assignment_name_1}: {assignment_grade_1}")
+print(f"{assignment_name_2}: {assignment_grade_2}")
+print(f"{assignment_name_3}: {assignment_grade_3}")
+print("--------------------------------")
+print(f"Total Score: {total_score}")
+print(f"Average Score: {average_score}")
 
 
